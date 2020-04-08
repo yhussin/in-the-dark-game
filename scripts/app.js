5e7f86787de5476d0d1580bc224dc52699638226
yhussin <yhussin@gmail.com>
2020-04-07 23:00:02 -0700

    adding CSS

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,4 @@
 
-let letsee = document.querySelector('.playerIcon')
-console.log("Is it working" + letsee)
-
 //code snippet: Consistently Move an Element with Arrow Keys in JavaScript
 document.onkeydown = detectKey;
 function detectKey(e) {
@@ -26,69 +23,50 @@ function detectKey(e) {
     }
 
     let player = document.querySelector('.playerIcon');
-    let positionPlayer = player.getBoundingClientRect(); 
+    let positionPlayer = player.getBoundingClientRect();
     console.log("player is at " + positionPlayer.x, positionPlayer.y)
 
-//collision detection
-    function collision () {
-        let rect1 = {x: positionPlayer.x, y: positionPlayer.y, width: 10, height: 10};
+    //collision detection
+    //From MDN
+    function collision() {
+        let rect1 = { x: positionPlayer.x, y: positionPlayer.y, width: 10, height: 10 };
         //var rect1 = {x: 5, y: 5, width: 50, height: 50};
-        let rect2 = {x: 348, y: 334, width: 10, height: 10};
+        let rect2 = { x: 348, y: 334, width: 50, height: 50 };
         //let rect2 = {x: 20, y: 10, width: 10, height: 10};
-    
+        let rect3 = { x: 648, y: 524, width: 50, height: 50 };
+
         if (rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.y + rect1.height > rect2.y) {
+            rect1.x + rect1.width > rect2.x &&
+            rect1.y < rect2.y + rect2.height &&
+            rect1.y + rect1.height > rect2.y) {
             // collision detected!
             console.log("collision is here")
-            let playerWhenHit = document.querySelector(".playerIcon");
-            playerWhenHit.classList.add("hitClass");
+            let obstacleWhenHit = document.querySelector(".obstacle");
+            obstacleWhenHit.classList.add("obstacleHit");
         }
-    
+
+        if (rect1.x < rect3.x + rect3.width &&
+            rect1.x + rect1.width > rect3.x &&
+            rect1.y < rect3.y + rect3.height &&
+            rect1.y + rect1.height > rect3.y) {
+            // collision detected!
+            console.log("second collision is here")
+            let goalWhenHit = document.querySelector(".goal");
+            goalWhenHit.classList.add("goalHit");
+        };
+
         // filling in the values =>
         if (5 < 30 &&
             55 > 20 &&
             5 < 20 &&
             55 > 10) {
             // collision detected!
-            //add class to .playerIcon
-            console.log("collision")
-        }
+        };
     };
-   collision()
+    collision()
 };
 
 // let player1 = document.querySelector('.playerIcon');
 // let positionPlayer1 = player1.getBoundingClientRect();
 
 
-/* 
-//collision detection
-//from MDN
-function collision () {
-    let rect1 = {x: positionPlayer1.x, y: positionPlayer1.y, width: 10, height: 10};
-    //var rect1 = {x: 5, y: 5, width: 50, height: 50};
-    let rect2 = {x: 348, y: 334, width: 10, height: 10};
-    //let rect2 = {x: 20, y: 10, width: 10, height: 10};
-
-    if (rect1.x < rect2.x + rect2.width &&
-    rect1.x + rect1.width > rect2.x &&
-    rect1.y < rect2.y + rect2.height &&
-    rect1.y + rect1.height > rect2.y) {
-        // collision detected!
-        console.log("collision is here")
-    }
-
-    // filling in the values =>
-    if (5 < 30 &&
-        55 > 20 &&
-        5 < 20 &&
-        55 > 10) {
-        // collision detected!
-        //add class to .playerIcon
-        console.log("collision")
-    }
-};
-
-//collision();  */
